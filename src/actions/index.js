3963e75aa80abc06bc63ec0d49f115d021c8cd63
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-09-15 15:42:40 -0500

    Add api request to action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
-export const FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL ='https://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=unhommequidort';
 
 export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
   return {
-    type: FETCH_POSTS
+    type: FETCH_POSTS,
+    payload: request
   };
 }
