f639b956d714086ad7f8bbf497d06663d53369cf
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-09-17 11:41:00 -0500

    Connect component to posts application state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,7 +4,7 @@ import { FETCH_POSTS } from '../actions';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      return _mapKeys(action.payload.data, 'id');
+      return _.mapKeys(action.payload.data, 'id');
     default:
       return state;
   }
