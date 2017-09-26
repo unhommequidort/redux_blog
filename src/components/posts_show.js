55acd9fa871550553d5ec9e82120297a671c32af
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-09-26 14:31:47 -0500

    Create link to index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
@@ -9,9 +10,18 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return (<div>Loading...</div>);
+    }
+
     return(
       <div>
-        PostsShow!
+        <Link to="/">Back to Index</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
