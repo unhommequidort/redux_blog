aadb83569e93ead25bedf2f44607aedf1e17b3cf
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-09-26 19:19:03 -0500

    Fire callback function after deletePost api call finishes

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,8 +36,9 @@ export function fetchPost(id) {
   }
 }
 
-export function deletePost(id) {
-  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(callback);
 
   return {
     type: DELETE_POST,
