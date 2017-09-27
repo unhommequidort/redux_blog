e94529b73b59689954b22f9661d2b627c46fe803
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-09-26 19:46:52 -0500

    Clean up comments

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -7,10 +7,6 @@ export default function(state = {}, action) {
       return _.omit(state, action.payload);
     case FETCH_POST:
       const post = action.payload.data;
-      // const newState = { ...state  };
-      // newState[post.id] = post;
-      // return newState;
-
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
