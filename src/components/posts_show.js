4e474b44eae7aa73a080bc0542bed70e701ecc81
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-09-26 19:17:03 -0500

    Pass redirect callback function to onDeleteClick

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -11,7 +11,9 @@ class PostsShow extends Component {
 
   onDeleteClick() {
     const { id } = this.props.match.params;
-    this.props.deletePost(id);
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
