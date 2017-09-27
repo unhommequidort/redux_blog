d4e6e35b6ec14557735bf0998c655befe2f181b4
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-09-26 19:14:04 -0500

    Change deletePost payload to id

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL ='https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=unhommequidort';
@@ -32,5 +33,14 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
+  }
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
+    payload: id
   }
 }
